02f62aadcf22322ed8d792ac2503b692e754f050
Omri Lauenger <omri@fireblocks.com>
2022-05-03 21:16:08 +0300

    fix Prettier

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -24,7 +24,7 @@ const LoggedOutView = (props) => {
 };
 
 const LoggedInView = (props) => {
-  if (props.currentUser) {
+  if (props?.currentUser) {
     return (
       <ul className="navbar-nav ml-auto">
         {props.currentUser.role === "admin" && (
@@ -48,8 +48,9 @@ const LoggedInView = (props) => {
               className="user-pic pr-1"
               alt={props.currentUser.username}
               onError={({ currentTarget }) => {
-                currentTarget.onerror = null; // prevents looping
-                currentTarget.src='https://static.productionready.io/images/smiley-cyrus.jpg';
+                currentTarget.onerror = null;
+                currentTarget.src =
+                  "https://static.productionready.io/images/smiley-cyrus.jpg";
               }}
             />
             {props.currentUser.username}
