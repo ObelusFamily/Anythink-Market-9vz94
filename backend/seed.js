2aada6f7c19e00cbf6865613fc4800f77bf5a2c0
Omri Lauenger <omri@fireblocks.com>
2022-05-06 21:32:24 +0300

    update mngoose connection

--- backend/seed.js
+++ backend/seed.js
@@ -1,28 +1,24 @@
 require("dotenv").config();
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI);
-
 
 require("./models/User");
 require("./models/Item");
 var Item = mongoose.model("Item");
 var User = mongoose.model("User");
 
-
 function saveItem(num) {
-    User.findById('627139709177580048e354b9')
-    .then(function(user) {
-    var tmpItem = {
-        title: 'item ' + num
-    }
+  User.findById("1").then(function (user) {
+    if (!user) {
+      var tmpItem = {
+        title: "item " + num,
+      };
       var item = new Item(tmpItem);
       item.seller = user;
       item.save();
-    });
-   
+    }
+  });
 }
-for(var i = 101; i < 102; i++){
-    saveItem(i);
-
+for (var i = 101; i < 102; i++) {
+  saveItem(i);
 }
-
