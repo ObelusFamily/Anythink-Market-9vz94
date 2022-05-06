8a936cf012b50f370f81625e8955625d1f80de09
Omri Lauenger <omri@fireblocks.com>
2022-05-06 22:10:27 +0300

    exit proccess once completed

--- backend/seed.js
+++ backend/seed.js
@@ -7,18 +7,17 @@ require("./models/Item");
 var Item = mongoose.model("Item");
 var User = mongoose.model("User");
 
-function saveItem(num) {
-  User.findOne().then(function (user) {
-    if (user) {
-      var tmpItem = {
-        title: "item " + num,
-      };
-      var item = new Item(tmpItem);
-      item.seller = user;
-      item.save();
-    }
-  });
+
+for (var i = 0; i < 100; i++) {
+    User.findOne().then(function (user) {
+        if (user) {
+          var tmpItem = {
+            title: "item " + i,
+          };
+          var item = new Item(tmpItem);
+          item.seller = user;
+          item.save();
+        }
+      });
 }
-for (var i = 101; i < 102; i++) {
-  saveItem(i);
-}
+process.exit(0);
