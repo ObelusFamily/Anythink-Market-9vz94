7ba7ebe83c094362baafdad8e8578877c1b56aa2
Omri Lauenger <omri@fireblocks.com>
2022-05-06 22:34:53 +0300

    last commit

--- backend/seed.js
+++ backend/seed.js
@@ -1,24 +1,27 @@
-// require("dotenv").config();
-// var mongoose = require("mongoose");
-// mongoose.connect(process.env.MONGODB_URI);
+require("dotenv").config();
+var mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI);
 
-// require("./models/User");
-// require("./models/Item");
-// var Item = mongoose.model("Item");
-// var User = mongoose.model("User");
+require("./models/User");
+require("./models/Item");
+var Item = mongoose.model("Item");
+var User = mongoose.model("User");
 
+var user = new User();
 
-// for (var i = 0; i < 100; i++) {
-//     User.findOne().then(function (user) {
-//         if (user) {
-//           var tmpItem = {
-//             title: "item " + i,
-//           };
-//           var item = new Item(tmpItem);
-//           item.seller = user;
-//           item.save();
-//         }
-//       });
-// }
-// process.exit(0);
-console.log('Success!')
+user.username = "xomri3";
+user.email = "xomri3@alink.co.il";
+user.setPassword("12345678");
+user.save().then(function () {
+  for (var i = 0; i < 101; i++) {
+    var tmpItem = {
+      title: "newitem " + i,
+    };
+    var item = new Item(tmpItem);
+    item.seller = user;
+    item.save().then(() => {
+      process.exit(0);
+    });
+  }
+});
+
