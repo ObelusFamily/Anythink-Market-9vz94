954233b97f443f0e80602b7145137038c4bd9ec4
Omri Lauenger <omri@fireblocks.com>
2022-05-06 22:13:50 +0300

    exit proccess once completed

--- backend/seed.js
+++ backend/seed.js
@@ -1,23 +1,24 @@
-require("dotenv").config();
-var mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI);
+// require("dotenv").config();
+// var mongoose = require("mongoose");
+// mongoose.connect(process.env.MONGODB_URI);
 
-require("./models/User");
-require("./models/Item");
-var Item = mongoose.model("Item");
-var User = mongoose.model("User");
+// require("./models/User");
+// require("./models/Item");
+// var Item = mongoose.model("Item");
+// var User = mongoose.model("User");
 
 
-for (var i = 0; i < 100; i++) {
-    User.findOne().then(function (user) {
-        if (user) {
-          var tmpItem = {
-            title: "item " + i,
-          };
-          var item = new Item(tmpItem);
-          item.seller = user;
-          item.save();
-        }
-      });
-}
-process.exit(0);
+// for (var i = 0; i < 100; i++) {
+//     User.findOne().then(function (user) {
+//         if (user) {
+//           var tmpItem = {
+//             title: "item " + i,
+//           };
+//           var item = new Item(tmpItem);
+//           item.seller = user;
+//           item.save();
+//         }
+//       });
+// }
+// process.exit(0);
+console.log('Success!')
