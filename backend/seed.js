be88bca9b5879cf9a1d12e437f7c56c8004b48eb
Omri Lauenger <omri@fireblocks.com>
2022-05-07 08:40:51 +0300

    seed db

--- backend/seed.js
+++ backend/seed.js
@@ -1,41 +1,71 @@
-require("dotenv").config();
-var mongoose = require("mongoose");
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+
 mongoose.connect(process.env.MONGODB_URI);
+require('./models/User');
+require('./models/Item');
+require('./models/Comment');
 
-require("./models/User");
-require("./models/Item");
-var Item = mongoose.model("Item");
-var User = mongoose.model("User");
-makeSeed();
+const User = mongoose.model('User');
+const Item = mongoose.model('Item');
+const Comment = mongoose.model('Comment');
 
-async function makeSeed(){
-    for(var n=0; n<101;n++){
-        var user = await saveUser(n)
-        await user.save().then(async function () {
-            for (var i = 0; i < 101; i++) {
-              await saveItem(user, i)
-            }
-        })
-    }
-    process.exit(0);
+async function createUser() {
+  const username = crypto.randomUUID().split('-')[0];
+  const pass = crypto.randomUUID().split('-')[1];
+  const user = new User();
+  user.username = `test${username}`;
+  user.email = `test+${username}@test.com`;
+  user.setPassword(pass);
+  try {
+    await user.save();
+    return user._id;
+  } catch (err) {
+    console.log(err, "Error creating user");
+  }
 }
 
-
-async function saveUser(i) {
-    var user = new User();
-
-    user.username = `myuser${i}`;
-    user.email = `myemailuser${i}@mydomain.com`;
-    user.setPassword("12345678");
-    return user;
+async function createItem(userId) {
+  const uuid = crypto.randomUUID().split('-')[0];
+  const item = new Item();
+  item.slug = `itemSlug${uuid}`;
+  item.title = 'just tile';
+  item.description = 'some description';
+  item.image = 'https://picsum.photos/536/354';
+  item.tagList = ['test1', 'test2'];
+  item.seller = userId;
+  try {
+    await item.save();
+    return item._id;
+  } catch (err) {
+    console.log(err, "Error createing item");
+  }
 }
 
-async function saveItem(user,i){
-    var tmpItem = {
-        title: "verynewitem " + i,
-      };
-      var item = new Item(tmpItem);
-      item.seller = user;
-      await item.save()
+async function createComment(userId, itemId) {
+  const comment = new Comment();
+  comment.body = 'some comment';
+  comment.seller = userId;
+  comment.item = itemId;
+  try {
+    await comment.save();
+  } catch (err) {
+    console.log(err, "Error creating comment");
+  }
 }
 
+async function populate() {
+  let counter = 0;
+  for (let i = 0; i < 100; i++) {
+    const userId = await createUser();
+    const itemId = await createItem(userId);
+    await createComment(userId, itemId);
+    counter++;
+  }
+  console.log(`done seed database with 100 users, items and comments`);
+}
+
+(async () => {
+  await populate();
+  process.exit();
+})();
